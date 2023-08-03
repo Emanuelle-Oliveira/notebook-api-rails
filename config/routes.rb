912d78be8e35f41504c9d09aafd4965870637746
Emanuelle Oliveira <emanuelletrindadeoliveira@gmail.com>
2023-08-03 20:05:34 -0300

    feat: http header version

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :kinds
 
   # scope module: 'v1' do
-  api_version(:module => 'V1', :parameter => {:name => 'version', :value => '1'}) do
+  # api_version(:module => 'V1', :parameter => {:name => 'version', :value => '1'}) do
+  # api_version(:module => 'V1', :header => {:name => 'Accept', :value => 'application/vdn.api+json; version=1'}) do
+  api_version(:module => 'V1', :header => {:name => 'X-Version', :value => '1.0'}) do
     resources :contacts do # , :constraints => lambda { |request| request.params[:version] == '1' } do
       resource :kind, only: [:show] # contacts/1/kind
       resource :kind, only: [:show], path: 'relationships/kind'
@@ -23,7 +25,9 @@ Rails.application.routes.draw do
   end
 
   # scope module: 'v2' do
-  api_version(:module => 'V2', :parameter => {:name => 'version', :value => '2'}) do
+  # api_version(:module => 'V2', :parameter => {:name => 'version', :value => '2'}) do
+  # api_version(:module => 'V2', :header => {:name => 'Accept', :value => 'application/vdn.api+json; version=2'}) do
+  api_version(:module => 'V2', :header => {:name => 'X-Version', :value => '2.0'}) do
     resources :contacts do # , :constraints => lambda { |request| request.params[:version] == '2' } do
       resource :kind, only: [:show] # contacts/1/kind
       resource :kind, only: [:show], path: 'relationships/kind'
